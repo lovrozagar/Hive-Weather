690e9e77fbcb09e81ac0f82e70e44d3ef7b54f10
lovro <lovro.zagar5@gmail.com>
2023-05-15 15:32:17 -0400

    feat: add Docs text content

--- src/scenes/docs/Docs.jsx
+++ src/scenes/docs/Docs.jsx
@@ -11,8 +11,11 @@ import {
 import { Anchor } from '@mui/icons-material'
 import FlexBox from '../../components/FlexBox'
 import GridBox from '../../components/GridBox'
+import { useTheme } from '@emotion/react'
 
 function Docs() {
+  const theme = useTheme().palette
+
   const content = [
     {
       title: 'Navigacijaska Traka',
@@ -30,6 +33,77 @@ function Docs() {
         {
           name: 'Popularne lokacije kartica',
           text: 'Prikazuje gumbe u obliku gradova, uvijek su tipa "week" te se klikom na gumb otvara grad sa tjednom prognozom. Popularni gradovi su fiksirani i ne mijenjaju se.',
+        },
+      ],
+    },
+    {
+      title: '1. Ekran (Home)',
+      id: 'screen-1',
+      paragraphs: [
+        {
+          name: 'Trenutna lokacija i vrijeme kartica',
+          text: 'Prikazuje trenutne vremenske uvjete na korisnikovoj lokaciji ako je pristup lokaciji prihvaćen, ili karticu koja obavještava da je lokacija isključena ako pristup nije odobren.',
+          api: 'JavaScript Geolocation, Google Geolocation, Open-Meteo Current',
+        },
+        {
+          name: 'Spremljene lokacije kartica',
+          text: 'Prikazuje gumbe u obliku gradova (gradovi se mogu spremati na 2. ili 3. ekranu), mogu biti tipa "tjedan" ili "dan", a klikom na gumb otvara se grad s tjednom ili dnevnom prognozom. Spremljene lokacije žive u "localStorage" i mogu se izbrisati klikom na gumb "remove" koji se nalazi na desnoj strani grada/gumba.',
+        },
+        {
+          name: 'Popularne lokacije kartica',
+          text: 'Prikazuje gumbe u obliku gradova, uvijek su tipa "week" te se klikom na gumb otvara grad sa tjednom prognozom. Popularni gradovi su fiksirani i ne mijenjaju se.',
+        },
+      ],
+    },
+    {
+      title: '2. Ekran (Forecast)',
+      id: 'screen-2',
+      paragraphs: [
+        {
+          name: 'Tražena lokacija, spremanje i dijeljenje - kartica',
+          text: 'Prikazuje lokacijske informacije o gradu. Takoder sadrži srce gumb s kojim se link lokacija tipa "week" sprema u (ili miče iz) "localStorage" i "saved" odjeljak na 1. ekranu. Kartica sadrži i gumb "share" koji prilikom klika koristi Web share API ako je podržan ili, ako nije podržan, kopira link u meduspremnik.',
+          api: 'Web Share API, Open-Meteo Forecast',
+        },
+        {
+          name: 'Dnevne kartice',
+          text: 'Prikazuju dan, datum, logo vremena, trajanje dana, temperaturu te dinamički obojani gradijent sukladno maksimalnoj i minimalnoj dnevnoj temperaturi. Pritiskom na gumb "See hourly" otvara se 24 satna prognoza za odabrani dan.',
+          api: 'Open-Meteo Forecast',
+        },
+        {
+          name: 'Satelitska karta grada',
+          text: 'Googleova satelitska karta s označenim traženim gradom na mapi, sadrži pune funkcije mape, slučajni "scroll" je onemogućen.',
+          api: 'Google Maps API',
+        },
+        {
+          name: 'Trenuto vrijeme kartica',
+          text: 'Prikazuje trenutno vrijeme grada: temperaturu, opis vremena, smjer puhanja vjetra, brzinu vjetra u obliku ikone i paragrafa.',
+          api: 'Open-Meteo Current',
+        },
+        {
+          name: '"Throwback" vrijeme kartica',
+          text: 'Prikazuje temperaturu, opis vremena, smjer puhanja vjetra i brzinu vjetra u formi ikona ali i paragrafa za traženi grad, na isti dan i vrijeme nasumično 5-40 godina u prošlosti.',
+          api: 'Open-Meteo History',
+        },
+      ],
+    },
+    {
+      title: '3. Ekran (Hourly)',
+      id: 'screen-3',
+      paragraphs: [
+        {
+          name: 'Tražena lokacija, spremanje i dijeljenje - kartica',
+          text: 'Prikazuje lokacijske informacije o gradu. Takoder sadrži srce gumb s kojim se link lokacija tipa "week" sprema u (ili miče iz) "localStorage" i "saved" odjeljak na 1. ekranu. Kartica sadrži i gumb "share" koji prilikom klika koristi Web share API ako je podržan ili, ako nije podržan, kopira link u meduspremnik.',
+          api: 'Web Share API, Open-Meteo Forecast',
+        },
+        {
+          name: 'Graf temperatura/sati u danu',
+          text: 'Prikzauje razinu temperature po satima za odabrani dan. Također sadrži vertikalnu liniju koja precizno označava trenutno vrijeme u traženom gradu. Graf je ineraktivan te se slika grafa može spremiti.',
+          packages: 'Moment, Moment-tz, Apex-charts',
+        },
+        {
+          name: 'Satne kartice',
+          text: 'Detaljno prikazuju vrijeme za određeni sat u obliku ikona i paragrafa koji se dinamički prilagođava i opisuju vrijeme ovisno o vremenskim uvjetima. Sadrže sljedeće informacije: temperaturu, temperaturu za ugodan osjećaj, opis vremena, mogućnost kiše, omjer naoblake, smjer vjetra, brzinu vjetra, vidljivost, atmosferski tlak, vlažnost, UV indeks i količinu snježnih oborina. Pritiskom na strelicu, pokazu/sakriju se detaljne informacije.',
+          api: 'Open-Meteo Hourly',
         },
       ],
     },
@@ -96,7 +170,10 @@ function Docs() {
                         <FlexBox mt={2} gap={1}>
                           <Typography
                             key={`${paragraph.name}${paragraph?.api}`}
-                            color={'primary.dark'}
+                            color={'tone.light'}
+                            sx={{
+                              textShadow: `1px 1px 15px ${theme.tone.lightLow}`,
+                            }}
                           >
                             {`${
                               paragraph.api.split(',').length > 1
@@ -110,7 +187,10 @@ function Docs() {
                         <FlexBox mt={2} gap={1}>
                           <Typography
                             key={`${paragraph.name}${paragraph?.packages}`}
-                            color={'primary.dark'}
+                            color={'tone.light'}
+                            sx={{
+                              textShadow: `1px 1px 15px ${theme.tone.lightLow}`,
+                            }}
                           >
                             {`${
                               paragraph.packages.split(',').length > 1
