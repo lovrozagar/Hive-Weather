c4236155d0e7cdc343453329e1ed3e98cdc50a4f
lovro <lovro.zagar5@gmail.com>
2023-05-15 03:45:43 -0400

    fix: get matching day instead of week day -1

--- src/utils/getDayOfWeek.js
+++ src/utils/getDayOfWeek.js
@@ -3,13 +3,13 @@ function getDayOfWeek(date_yyyy_mm_dd, index) {
   if (index === 1) return 'Tomorrow'
 
   const daysOfWeek = [
+    'Sunday',
     'Monday',
     'Tuesday',
     'Wednesday',
     'Thursday',
     'Friday',
     'Saturday',
-    'Sunday',
   ]
   const date = new Date(date_yyyy_mm_dd)
 
