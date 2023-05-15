2c0dab5a23eabcb7d5dd96189c263062ebc6b0a6
lovro <lovro.zagar5@gmail.com>
2023-05-15 15:43:00 -0400

    refactor: swap useNavigate navigate function for Link component, add Docs button

--- src/scenes/appBar/AppBar.jsx
+++ src/scenes/appBar/AppBar.jsx
@@ -1,64 +1,77 @@
-import { Search, Menu } from '@mui/icons-material'
 import {
   Box,
-  useTheme,
-  useMediaQuery,
   AppBar as Bar,
   Toolbar,
+  Button,
   IconButton,
-  Typography,
+  Zoom,
 } from '@mui/material'
-import React from 'react'
+import { DarkModeOutlined, LightModeOutlined } from '@mui/icons-material'
+import FlexBox from '../../components/FlexBox'
+import AppTitle from './AppTitle'
 import AutocompleteCity from '../home/AutocompleteCity'
-import ModeSwitch from './ModeSwitch'
+import SideMenu from './SideMenu'
+
+import { ModeContext } from '../../App'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 function AppBar() {
-  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
-  const theme = useTheme()
-  const neutralLight = theme.palette.neutral.light
-  const dark = theme.palette.neutral.dark
-  const alt = theme.palette.background.alt
+  const { mode, handleModeSwitch } = useContext(ModeContext)
+
+  const appBarStyle = {
+    position: 'relative',
+    zIndex: 20,
+    mb: 3,
+  }
+  const toolbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    minHeight: 64,
+    gap: { xs: 1, sm: 2, lg: 0 },
+  }
+  const titleStye = { flex: { xs: 0, sm: 0, lg: 1 }, mr: 'auto' }
+  const autocompleteStyle = { flex: 1, minWidth: { lg: 750 } }
+  const optionsStyle = { flex: { xs: 0, sm: 0, lg: 1 }, ml: 'auto' }
+  const optionButtonStyle = { display: { xs: 'none', md: 'inline-flex' } }
 
   return (
-    <Bar position='static' color='navbar'>
-      <Toolbar
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          gap: 1,
-        }}
-      >
-        <Typography
-          variant='h6'
-          noWrap
-          component='div'
-          sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-          Hive Weather
-        </Typography>
-        <Box
-          color='navbar'
-          sx={{
-            width: '50%',
-            mx: 2,
-            m: '0 auto',
-            // display: { xs: 'none', sm: 'block' },
-            color: 'white',
-          }}
-        >
+    <Bar sx={appBarStyle}>
+      <Toolbar sx={toolbarStyle}>
+        <AppTitle component='h1' main sx={titleStye} />
+        <Box color='navbar' sx={autocompleteStyle}>
           <AutocompleteCity />
         </Box>
-        <Box position='relative'>
-          <ModeSwitch />
-        </Box>
-        <IconButton
-          size='large'
-          edge='start'
-          color='inherit'
-          aria-label='open drawer'
-        >
-          <Menu />
-        </IconButton>
+        <FlexBox type='end' gap={0.5} sx={optionsStyle}>
+          <Box sx={optionButtonStyle}>
+            <Link to='/hive-weather/docs'>
+              <Button color='lightPreserved' sx={{ mr: 1 }}>
+                Docs
+              </Button>
+            </Link>
+          </Box>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleModeSwitch}
+            sx={optionButtonStyle}
+          >
+            {/* WRITTEN IN THIS WAY SO THAT THE ZOOM ANIMATIONS TRIGGER ON CHANGE */}
+            {mode && mode === 'dark' && (
+              <Zoom appear in={true}>
+                <LightModeOutlined />
+              </Zoom>
+            )}
+            {mode !== 'dark' && (
+              <Zoom appear in={true}>
+                <DarkModeOutlined />
+              </Zoom>
+            )}
+          </IconButton>
+          <SideMenu />
+        </FlexBox>
       </Toolbar>
     </Bar>
   )
