b232106360ff640a2423562394a5c657365d0139
lovro <lovro.zagar5@gmail.com>
2023-05-16 19:12:22 -0400

    refactor: organize autocomplete input in a separate component

---
+++ src/scenes/home/InputAutocompleteField.jsx
@@ -0,0 +1,37 @@
+import { TextField, CircularProgress } from '@mui/material'
+import { Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+InputAutocompleteField.propTypes = {
+  loading: PropTypes.bool.isRequired,
+}
+
+function InputAutocompleteField({ loading, ...params }) {
+  return (
+    <TextField
+      {...params}
+      color='primary'
+      size='small'
+      placeholder='Search a city'
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            border: 'none',
+          },
+        },
+      }}
+      InputProps={{
+        ...params.InputProps,
+        endAdornment: (
+          <Fragment>
+            {loading ? <CircularProgress color='inherit' size={20} /> : null}
+            {params.InputProps.endAdornment}
+          </Fragment>
+        ),
+      }}
+    />
+  )
+}
+
+export default InputAutocompleteField
