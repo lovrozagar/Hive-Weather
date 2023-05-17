c30272a4c25b267ecba65d91f3ef5ca828e5e6bb
lovro <lovro.zagar5@gmail.com>
2023-05-17 09:51:56 -0400

    style: make chart annotation follow the app light/dark mode colors

--- src/scenes/hourly/Hourly.jsx
+++ src/scenes/hourly/Hourly.jsx
@@ -1,21 +1,37 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import useFetchHourlyData from '../../api/useFetchHourlyData'
-import { Box, Container, Skeleton } from '@mui/material'
+import { useTheme, Box, Container, Skeleton } from '@mui/material'
 import GridBox from '../../components/GridBox'
+import CityTitle from '../../components/CityTitle'
+import HourCard from './HourCard'
+
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ModeContext } from '../../App'
 import moment from 'moment-timezone'
 import ReactApexChart from 'react-apexcharts'
-import HourCard from './HourCard'
-import ThrowbackCard from '../forecast/ThrowbackCard'
+import useFetchHourlyData from '../../api/useFetchHourlyData'
 
 function Hourly() {
-  const { city, timezone, latitude, longitude, dayIndex } = useParams()
+  const { mode } = useContext(ModeContext)
+  const theme = useTheme().palette
+
+  const { city, country, countryCode, timezone, latitude, longitude, day } =
+    useParams()
 
   const lat = useMemo(() => parseFloat(latitude.replace('_', '.')), [latitude])
   const lng = useMemo(
     () => parseFloat(longitude.replace('_', '.')),
     [longitude]
   )
+  const timezoneTzFormat = useMemo(
+    () => timezone.split('-').join('/'),
+    [timezone]
+  )
+  const dayIndex = useMemo(() => {
+    if (day === 'today') return 0
+    if (day === 'tomorrow') return 1
+    else return day.split('-')[1]
+  }, [day])
+
   const [currentTime, setCurrentTime] = useState(null)
   const [options, setOptions] = useState(null)
 
@@ -25,31 +41,32 @@ function Hourly() {
   const TWO_HOUR_MILLISECOND_OFFSET = 7_200_000
 
   const getCurrentTimezoneMilliseconds = useCallback(() => {
-    const now = moment().tz(timezone.split('_').join('/'))
+    const now = moment().tz(timezoneTzFormat)
     const offset = now._offset * MINUTE_TO_MILLISECOND_MULTIPLAYER
 
     return Date.now() + offset - TWO_HOUR_MILLISECOND_OFFSET
-  }, [timezone])
+  }, [timezoneTzFormat])
 
   const chartOptions = useMemo(() => {
     if (!hours) return null
     return {
       chart: {
         type: 'area',
+        foreColor: theme.tone.light,
+      },
+      tooltip: {
+        theme: mode,
       },
       annotations: {
         xaxis: [
           {
             x: currentTime,
-            borderColor: '#000',
             label: {
-              borderColor: '#000',
-              style: {
-                fontSize: '12px',
-                color: '#fff',
-                background: '#000',
-              },
               text: 'Location Current Time',
+              style: {
+                color: theme.tone.light,
+                background: theme.tone.dark,
+              },
             },
           },
         ],
@@ -68,7 +85,7 @@ function Hourly() {
         categories: hours.time || [],
       },
     }
-  }, [currentTime, hours])
+  }, [currentTime, hours, theme, mode])
 
   useEffect(() => {
     setCurrentTime(getCurrentTimezoneMilliseconds())
@@ -81,7 +98,16 @@ function Hourly() {
   }, [hours, currentTime, chartOptions])
 
   return (
-    <Container>
+    <Container component='main'>
+      <CityTitle
+        component='section'
+        city={city}
+        country={country}
+        countryCode={countryCode}
+        lat={lat.toString()}
+        lng={lng.toString()}
+        sx={{ mb: 2 }}
+      />
       {options ? (
         <ReactApexChart
           options={options}
@@ -98,7 +124,7 @@ function Hourly() {
           sx={{ mb: 2 }}
         />
       )}
-      <GridBox gap={1}>
+      <GridBox component='section' gap={1}>
         {hours
           ? hours.time.map((hour, index) => {
               return (
