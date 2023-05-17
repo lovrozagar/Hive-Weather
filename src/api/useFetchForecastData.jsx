30cbbe5c93f73cb5f7b2793172dba304e78e5531
lovro <lovro.zagar5@gmail.com>
2023-05-17 16:37:21 -0400

    refactor: connect hook for fethching forecast data to server

--- src/api/useFetchForecastData.jsx
+++ src/api/useFetchForecastData.jsx
@@ -6,16 +6,14 @@ const useFetchForecastData = (latitude, longitude) => {
   const fetchForecastData = useCallback(async () => {
     if (!latitude || !longitude) return
 
-    const prefix = 'https://api.open-meteo.com/v1/forecast?'
-    const lat = `latitude=${latitude}`
-    const lng = `&longitude=${longitude}`
-    const options =
-      '&current_weather=true&hourly=relativehumidity_2m,temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max&timezone=auto'
+    const backendAbsolutePath = 'http://localhost:5000/api/weather/forecast?'
 
     try {
-      const response = await fetch(`${prefix}${lat}${lng}${options}`)
+      const response = await fetch(
+        `${backendAbsolutePath}latitude=${latitude}&longitude=${longitude}`
+      )
       const json = await response.json()
-      console.log('FORECAST REQUEST', json)
+      console.log('FORECAST BACKEND REQUEST', json)
       setForecastData([json.timezone, json.current_weather, json.daily])
     } catch (error) {
       console.error(error)
