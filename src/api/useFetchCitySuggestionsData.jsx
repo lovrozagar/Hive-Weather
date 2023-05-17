6ca09ec8aedce309440613389f8a85d9b6c910aa
lovro <lovro.zagar5@gmail.com>
2023-05-17 11:40:46 -0400

    refactor: get API response through url fetch instead of using google service and CDN. This breaks autcomplete functionality on the client due to cors, start transition to backend from this point

--- src/api/useFetchCitySuggestionsData.jsx
+++ src/api/useFetchCitySuggestionsData.jsx
@@ -1,56 +1,80 @@
 import { useCallback } from 'react'
 
+// WILL NOT WORK DUE TO CORS, REWRITTEN TO THIS SO THAT I CAN REMOVE CDN AND START TRANSITIONING ON THE BACKEND
+
 const useFetchCitySuggestionsData = () => {
+  //
   const fetchCitySuggestionsData = useCallback(async (input) => {
-    const autocompleteService =
-      new window.google.maps.places.AutocompleteService()
-    const options = {
-      input,
-      types: ['(cities)'],
-    }
-    const results = await new Promise((resolve, reject) => {
-      autocompleteService.getPlacePredictions(options, (results, status) => {
-        if (status !== 'OK') {
-          resolve([])
+    try {
+      const prefix =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
+      const googleKey = import.meta.env.VITE_GOOGLE_API_KEY
+      const key = `key=${googleKey}`
+
+      const response = await fetch(
+        `${prefix}input=${input}&types=geocode&${key}`
+      )
+
+      const json = await response.json()
+
+      if (json.status === 'OK') {
+        const predictions = json.predictions
+        const suggestions = []
+
+        for (const prediction of predictions) {
+          const { place_id } = prediction
+          const placeDetails = await getPlaceDetails(place_id, googleKey)
+
+          if (placeDetails) {
+            const { city, country, countryCode, lat, lng } = placeDetails
+            suggestions.push({
+              name: city,
+              region: country,
+              countryCode,
+              lat,
+              lon: lng,
+            })
+          }
         }
-        resolve(results)
-      })
-    })
 
-    const suggestions = await Promise.all(
-      results.map(async (result) => {
-        const placeId = result.place_id
-        const details = await new Promise((resolve, reject) => {
-          const placesService = new window.google.maps.places.PlacesService(
-            document.createElement('div')
-          )
-          placesService.getDetails({ placeId }, (details, status) => {
-            if (status !== 'OK') {
-              reject(status)
-            }
-            resolve(details)
-          })
-        })
-
-        const city = result.structured_formatting.main_text || 'no-city'
-        const country =
-          result.structured_formatting.secondary_text || 'no-country' // RETURN EMPTY STRING IN KOSOVO LIKE CASES WHERE COUNTRY IS NOT RETURNED
-        const countryCode =
-          details.address_components.find((component) =>
-            component.types.includes('country')
-          )?.short_name || 'no-country-code'
-        const lat = details.geometry.location.lat()
-        const lng = details.geometry.location.lng()
-
-        console.log({ city, country, countryCode, lat, lng })
-        return { city, country, countryCode, lat, lng }
-      })
-    )
-
-    return suggestions
+        console.log(suggestions)
+        return suggestions
+      }
+      //
+    } catch (error) {
+      console.error('Error occurred:', error)
+    }
   }, [])
 
   return fetchCitySuggestionsData
+}
+
+async function getPlaceDetails(placeId, apiKey) {
+  const detailsUrl = 'https://maps.googleapis.com/maps/api/place/details/json?'
+  const key = `key=${apiKey}`
+
+  const response = await fetch(`${detailsUrl}place_id=${placeId}&${key}`)
+  const json = await response.json()
+
+  if (json.status === 'OK') {
+    const { address_components } = json.result
+    const cityComponent = address_components.find((component) =>
+      component.types.includes('locality')
+    )
+    const countryComponent = address_components.find((component) =>
+      component.types.includes('country')
+    )
+
+    if (cityComponent && countryComponent) {
+      const city = cityComponent.long_name
+      const country = countryComponent.long_name
+      const countryCode = countryComponent.short_name
+      const { lat, lng } = json.result.geometry.location
+      return { city, country, countryCode, lat, lng }
+    }
+  }
+
+  return null
 }
 
 export default useFetchCitySuggestionsData
